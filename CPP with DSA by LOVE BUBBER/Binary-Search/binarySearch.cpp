e9d9eb5e85867bccea5b0a798af084af41044861
hanzlikhan <khangormani79@gmail.com>
2024-04-24 20:55:46 +0500

    init

--- CPP with DSA by LOVE BUBBER/Binary-Search/binarySearch.cpp
+++ CPP with DSA by LOVE BUBBER/Binary-Search/binarySearch.cpp
@@ -4,11 +4,31 @@ int binarySearch(int arr[],int size,int key){
     int start = 0;
     int end = size-1;
     int mid = start + (end-start)/2;
+    while (start<=end)
+    {
+        if (arr[mid]== key)
+        {
+            return mid;
+        }
+        if (key>arr[mid])
+        {
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+        mid = start + (end-start)/2;
+        
+    }
+    return -1;
+    
 }
 int main(){
     int arr1[6] = {2,4,6,8,12,18};
     int arr2[5] = {3,5,7,9,11};
-
-    binarySearch(arr1,6,18);
-    binarySearch(arr2,5,10);
+    int result = binarySearch(arr1,6,18);
+    cout<<"index of 18 found at "<<result<<endl;
+    int res =  binarySearch(arr2,5,10);
+    cout<<"index of 10 found at "<<res<<endl;
+    return 0;
 }
