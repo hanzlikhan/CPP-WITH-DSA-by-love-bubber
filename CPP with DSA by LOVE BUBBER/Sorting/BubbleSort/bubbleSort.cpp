ec490217172680c01b956020857257976a5583b1
hanzlikhan <khangormani79@gmail.com>
2024-04-27 14:09:42 +0500

    init

--- CPP with DSA by LOVE BUBBER/Sorting/BubbleSort/bubbleSort.cpp
+++ CPP with DSA by LOVE BUBBER/Sorting/BubbleSort/bubbleSort.cpp
@@ -2,22 +2,20 @@
 #include <algorithm> // for std::swap
 using namespace std;
 
-void selectionSort(int arr[], int n) {
+void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        int mini = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[mini]) {
-                mini = j;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
             }
         }
-        swap(arr[mini], arr[i]);
     }
 }
 
 int main() {
     int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
-    selectionSort(arr, n);
+    bubbleSort(arr, n);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
