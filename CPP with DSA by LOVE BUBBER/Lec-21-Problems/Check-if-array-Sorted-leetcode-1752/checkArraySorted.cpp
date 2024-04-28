28351f0bc76cbe3ee323067963686526bf024379
hanzlikhan <khangormani79@gmail.com>
2024-04-28 05:43:13 +0500

    init

---
+++ CPP with DSA by LOVE BUBBER/Lec-21-Problems/Check-if-array-Sorted-leetcode-1752/checkArraySorted.cpp
@@ -0,0 +1,16 @@
+// leetcode  - 1752. Check if Array Is Sorted and Rotated
+// class Solution {
+// public:
+//     bool check(vector<int>& nums) {
+//         int cnt = 0;
+//         for(int i=1;i<nums.size();i++) 
+//         {
+//             if(nums[i] < nums[i-1])
+//                 cnt++;
+//         }
+//         if(nums[nums.size()-1] > nums[0])
+//             cnt++;
+        
+//         return cnt<=1;
+//     }
+// };
